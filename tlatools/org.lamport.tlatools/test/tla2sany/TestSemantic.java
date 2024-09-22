38ebc9d1cab7640d62de217266d8137a8543385b
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-09-22 15:37:31 -0400

    Added error logging to semantic runner test
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- tlatools/org.lamport.tlatools/test/tla2sany/TestSemantic.java
+++ tlatools/org.lamport.tlatools/test/tla2sany/TestSemantic.java
@@ -11,21 +11,44 @@ import tla2sany.configuration.Configuration;
 import tla2sany.parser.TLAplusParser;
 import tla2sany.semantic.AbortException;
 import tla2sany.semantic.BuiltInLevel;
+import tla2sany.semantic.Errors;
 import tla2sany.semantic.Generator;
 import tla2sany.semantic.ModuleNode;
+import tla2sany.semantic.SemanticNode;
+import tla2sany.st.TreeNode;
 
 public class TestSemantic {
-
-	@Test
-	public void generate() throws AbortException {
+	
+	private static TreeNode checkSyntax(String input) throws AbortException {
 		Configuration.load(null);
 		BuiltInLevel.load();
-		String input = "---- MODULE Test ----\nx == 5\n====";
 		byte[] inputBytes = input.getBytes(StandardCharsets.UTF_8);
 		InputStream inputStream = new ByteArrayInputStream(inputBytes);
 		TLAplusParser parser = new TLAplusParser(inputStream, StandardCharsets.UTF_8.name());
-		Assert.assertTrue(parser.parse());
-		Generator gen = new Generator(null, null);
-		ModuleNode semanticTree = gen.generate(parser.rootNode());
+		Assert.assertTrue(input, parser.parse());
+		return parser.rootNode();
+	}
+	
+	private static ModuleNode checkSemantic(TreeNode parseTree) throws AbortException {
+		Errors errors = new Errors();
+		Generator gen = new Generator(null, errors);
+		SemanticNode.setError(errors);
+		ModuleNode semanticTree = gen.generate(parseTree);
+		Assert.assertTrue(errors.toString(), errors.isSuccess());
+		Assert.assertNotNull(errors.toString(), semanticTree);
+		return semanticTree;
+	}
+	
+	private static void checkLevel(ModuleNode semanticTree) {
+		boolean result = semanticTree.levelCheck(1);
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void generate() throws AbortException {
+		String input = "---- MODULE Test ----\nx == ([]5) = 2\n====";
+		TreeNode parseTree = checkSyntax(input);
+		ModuleNode semanticTree = checkSemantic(parseTree);
+		checkLevel(semanticTree);
 	}
 }
