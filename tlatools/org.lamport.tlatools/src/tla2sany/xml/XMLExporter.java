7536dc0fb3bb0f32a92205b02b9d3ab5db513605
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-10-31 15:36:53 -0400

    SANY XML Exporter: added CLI help text and pretty-print option
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- tlatools/org.lamport.tlatools/src/tla2sany/xml/XMLExporter.java
+++ tlatools/org.lamport.tlatools/src/tla2sany/xml/XMLExporter.java
@@ -4,19 +4,24 @@
 package tla2sany.xml;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * a tool for exporting the loaded modules to XML format
  */
 
-import java.io.PrintStream;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.URL;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -42,51 +47,122 @@ import util.ToolIO;
 
 public class XMLExporter {
 
-  public static final void main(String[] args) throws XMLExportingException {
+  /**
+   * CLI help text listing various options.
+   */
+  private static final String HELP_TEXT =
+    "SANY XML Exporter\n" +
+    "Usage:\n\n" +
+    "  java tla2sany.xml.XMLExporter [-o|-p] [-I <dir_path>]* <input_file>.tla\n\n" +
+    "Options:\n" +
+    "  -o: Offline mode; skip XML schema validation step.\n" +
+    "  -p: Pretty-print; format XML output to be read by humans.\n" +
+    "  -I: Include; use given directory path to resolve module dependencies.\n" +
+    "  -h: Help; print this help text.\n";
 
-    // parse arguments, possible flag
-    // s
-    // -I (a modules path) can be repeated
-    // -o offline mode (no validation) //TODO: use a resolver to do offline validation
-    // then a list of top level modules to parse)
-    if (args.length < 1) throw new IllegalArgumentException("at least one .tla file must be given");
-    LinkedList pathsLs = new LinkedList();
+  /**
+   * The main entry-point to the XML exporter program. Parses the command-
+   * line args then calls the main run function. Terminates with 0 exit code
+   * if successful, 1 on failure.
+   *
+   * @param args The command-line args.
+   */
+  public static final void main(String[] args) {
+    try {
+      RunOptions options = parseArgs(args);
+      if (null == options) {
+        ToolIO.out.println(HELP_TEXT);
+      } else {
+        String result = run(options.OfflineMode, options.PrettyPrint, options.Include, options.TlaFile);
+        ToolIO.out.print(result);
+      }
+    } catch (IllegalArgumentException e) {
+      ToolIO.err.println("ERROR: " + e.getMessage() + "\n");
+      ToolIO.err.println(HELP_TEXT);
+      System.exit(1);
+    } catch (XMLExportingException e) {
+      ToolIO.err.println(e.toString());
+      System.exit(1);
+    }
+  }
 
+  static class RunOptions {
+    public final boolean OfflineMode;
+    public final boolean PrettyPrint;
+    public final String[] Include;
+    public final String TlaFile;
+    public RunOptions(boolean offlineMode, boolean prettyPrint, String[] include, String tlaFile) {
+      this.OfflineMode = offlineMode;
+      this.PrettyPrint = prettyPrint;
+      this.Include = include;
+      this.TlaFile = tlaFile;
+    }
+  }
+
+  /**
+   * Parses command-line args.
+   *
+   * @return Null if help request, {@link:RunOptions} instance otherwise.
+   * @throws IllegalArgumentException If args are invalid.
+   */
+  static RunOptions parseArgs(String[] args) {
+    List<String> paths = new ArrayList<String>();
     boolean offline_mode = false;
-    int lastarg = -1; // lastarg will be incremented, initialize at -1
-    for (int i = 0; i < args.length - 1; i++) {
+    boolean pretty_print = false;
+    String tla_name = null;
+    for (int i = 0; i < args.length; i++) {
       if ("-o".equals(args[i])) {
         offline_mode = true;
-        lastarg = i;
+      } else if ("-p".equals(args[i])) {
+        pretty_print = true;
+      } else if ("-h".equals(args[i])) {
+        return null;
       } else if ("-I".equals(args[i])) {
         i++;
-        if (i > args.length - 2)
-          throw new IllegalArgumentException("the -I flag must be followed by a directory and at least one .tla file");
-        pathsLs.addLast(args[i]);
-        lastarg = i;
+        if (i > args.length - 1) {
+          throw new IllegalArgumentException("The -I flag must be followed by a directory path.");
+        }
+        paths.add(args[i]);
+      } else {
+        if (null == tla_name) {
+          tla_name = args[i];
+        } else {
+          String message =
+            "Unrecognized command-line option " + args[i]
+            + "\nNote: only one TLA+ file can be translated per run.";
+          throw new IllegalArgumentException(message);
+        }
       }
     }
 
-    lastarg++;
+    if (null == tla_name) {
+      throw new IllegalArgumentException("At least one .tla file must be given.");
+    }
 
-    String[] paths = new String[pathsLs.size()];
-    for (int i = 0; i < paths.length; i++) paths[i] = (String) pathsLs.get(i);
+    return new RunOptions(offline_mode, pretty_print, paths.toArray(String[]::new), tla_name);
+  }
 
-    if (args.length - lastarg != 1)
-      throw new IllegalArgumentException("Only one TLA file to check allowed!");
-
-    String tla_name = args[lastarg++];
+  /**
+   * Parses the given TLA+ spec and outputs the parse tree as an XML string.
+   *
+   * @param offline_mode If true, skip validating XML against schema.
+   * @param pretty_print If true, print human-readable XML.
+   * @param paths Paths to use to search for module dependencies.
+   * @param tla_name The TLA+ spec to parse.
+   * @return Whether parsing & translation was successful.
+   * @throws IllegalArgumentException If cannot find files given in args.
+   * @throws XMLExportingException If serialization to XML failed.
+   */
+  public static String run(
+      boolean offline_mode,
+      boolean pretty_print,
+      String[] paths,
+      String tla_name
+    ) throws XMLExportingException {
 
     FilenameToStream fts = new SimpleFilenameToStream(paths);
 
-    // redirecting System.out
-    PrintStream out = System.out;
-    System.setOut(new PrintStream(new ByteArrayOutputStream()));
-
     SpecObj spec = new SpecObj(tla_name, fts);
-
-    // Print documentation line on System.out
-    ToolIO.out.println("\n****** SANY2 " + SANY.version + "\n");
 
     // Get next file name from command line; then parse,
     // semantically analyze, and level check the spec started in
@@ -95,21 +171,21 @@ public class XMLExporter {
     // check if file exists
     //ToolIO.out.println("Processing: "+tlas[i]+"\n"+(tlas[i] == null));
     if (FileUtil.createNamedInputStream(tla_name, spec.getResolver()) != null) {
+      PrintStream out = ToolIO.out;
+      ToolIO.out = new PrintStream(new ByteArrayOutputStream());
       try {
-        SANY.frontEndMain(spec, tla_name, System.err);
+        SANY.frontEndMain(spec, tla_name, ToolIO.out);
         if (spec.getExternalModuleTable() == null)
           throw new XMLExportingException("spec " + spec.getName() + " is malformed - does not have an external module table", null);
         if (spec.getExternalModuleTable().getRootModule() == null)
           throw new XMLExportingException("spec " + spec.getName() + " is malformed - does not have a root module", null);
       } catch (FrontEndException fe) {
-        // For debugging
-        fe.printStackTrace();
-        ToolIO.out.println(fe);
-        return;
+        throw new IllegalArgumentException(fe.toString());
+      } finally {
+        ToolIO.out = out;
       }
     } else {
-      ToolIO.out.println("Cannot find the specified file " + tla_name + ".");
-      return;
+      throw new IllegalArgumentException("Cannot find the specified file " + tla_name + ".");
     }
 
 
@@ -148,6 +224,10 @@ public class XMLExporter {
       //Create XML file
       TransformerFactory transformerFactory = TransformerFactory.newInstance();
       Transformer transformer = transformerFactory.newTransformer();
+      if (pretty_print) {
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+      }
       DOMSource source = new DOMSource(doc);
 
       // validate the file, do not fail if there is a URL connection error
@@ -156,8 +236,7 @@ public class XMLExporter {
           SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
           URL schemaFile = XMLExporter.class.getResource("sany.xsd");
           if (null == schemaFile) {
-            ToolIO.err.println("ERROR: Unable to find sany.xsd schema file that is expected to be embedded in the jar.");
-            System.exit(1);
+            throw new RuntimeException("Unable to find sany.xsd schema file that is expected to be embedded in the jar.");
           }
           Schema schema = factory.newSchema(schemaFile);
           // create a Validator instance, which can be used to validate an instance document
@@ -173,9 +252,11 @@ public class XMLExporter {
             // but fail for other errors
           }*/
       }
-      StreamResult result = new StreamResult(out);
+      StringWriter writer = new StringWriter();
+      StreamResult result = new StreamResult(writer);
 
       transformer.transform(source, result);
+      return writer.toString();
     } catch (ParserConfigurationException pce) {
       throw new XMLExportingException("failed to write XML", pce);
     } catch (TransformerException tfe) {
@@ -183,7 +264,6 @@ public class XMLExporter {
     } catch (SAXException se) {
       throw new XMLExportingException("failed to validate XML", se);
     }
-
   }
 
   static void insertRootName(Document doc, Element rootElement, SpecObj spec) {
